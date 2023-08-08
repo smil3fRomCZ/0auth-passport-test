40bce1549216d55b5de70b47b7affeec9bb6084b
Jan Melicherik <janmelicherik84@gmail.com>
2023-08-08 19:41:47 +0200

    set login page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 
 const homeRouter = require("./routes/homeRouter");
+const authRouter = require("./routes/authRouter");
 
 const app = express();
 
 // Set view engine
 app.set("view engine", "ejs");
 
+// Set routes
+app.use("/auth", authRouter);
 app.use("/", homeRouter);
 
 app.listen(3000, () => {
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,3 @@
 exports.getHomePage = (req, res) => {
-  res.render("home");
+  res.render("homePage");
 };
